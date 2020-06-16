9cac759c0808bf0fde3f2ea93d218433feaaaa03
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-06-16 14:18:30 -0300

    DOCKERFILE: Ajustado estrutura e criado dockerfile

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/vivalisto';
+const url = 'mongodb://dbuser:dbuser18@ds249267.mlab.com:49267/docker-db';
+// const url = 'mongodb://localhost:27017/vivalisto';
 
 mongoose.connect(url, { useNewUrlParser: true });
 
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,5 +4,6 @@ import UserController from '../controllers/UserController';
 const router = express.Router();
 
 router.post('/', UserController.create);
+router.get('/test', (req, res) => res.json({ msg: 'server ativo' }));
 
 export default router;
