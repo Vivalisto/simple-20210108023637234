dca1a3fa23d49eea6f56a41a21debab1de27242a
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-07-03 21:09:28 -0300

    DEBUG: Configurado debug

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import StartUp from './startUp';
 let port = process.env.PORT || '3333';
 
 StartUp.app.listen(port, () => {
-  console.log(`servidor executando na porta ${port}`);
+  console.log(`😀 servidor executando na porta ${port}`);
 });
 
 // import express, { json } from 'express';
