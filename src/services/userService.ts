807c3ddf31888be8f5662caa5f0a717d95959511
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-08-03 23:14:57 -0300

    Ajustado service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -68,7 +68,7 @@ class UserService {
 
     Mail.to = user.email;
     Mail.subject = 'Redefinição senha sistema Vivalisto';
-    Mail.message = `Solicitação de alteração de senha. <a href=http://localhost:3000/reset-password/${user.email}/${token}> Clique aqui para alterar sua senha</a>`;
+    Mail.message = `Solicitação de alteração de senha. <a href=http://150.238.42.242:30080/reset-password/${user.email}/${token}> Clique aqui para alterar sua senha</a>`;
     await Mail.sendMail();
 
     return;
