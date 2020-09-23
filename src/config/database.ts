18bedb6aa85151a7e1f842fde6438a4631e2a147
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-09-22 23:05:56 -0300

    CONEXAO COM IBM

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,7 +5,17 @@ class DataBase {
   private DB_URL = keys.mongoURI;
 
   createConnection() {
-    mongoose.connect(this.DB_URL, { useNewUrlParser: true });
+    mongoose
+      .connect(this.DB_URL, {
+        useNewUrlParser: true,
+        tls: true,
+        tlsCAFile: `src/config/certificado/48b1ff99-7b37-4370-b9e6-229f4b939777`,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('Conexão com o db realizada com sucesso'))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 
--- src/config/keys-dev.ts
+++ src/config/keys-dev.ts
@@ -1,9 +1,10 @@
 const keys = {
   mongoURI:
-    'mongodb://dbuser:vivalisto2020@ds125716.mlab.com:25716/vivalisto-dev',
+    'mongodb://admin:vivalisto2020@7c6a2ac2-2497-4be6-9444-1efbcacaa0a6-0.br37s45d0p54n73ffbr0.databases.appdomain.cloud:30902,7c6a2ac2-2497-4be6-9444-1efbcacaa0a6-1.br37s45d0p54n73ffbr0.databases.appdomain.cloud:30902/vivalisto-dev?authSource=admin&replicaSet=replset',
   secretOrKey: '9f920d6932f46a4a1fc98d25ffbbb166', //MD5
   apiKeySendgrid:
     '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',
 };
 
+//mlab 'mongodb://dbuser:vivalisto2020@ds125716.mlab.com:25716/vivalisto-dev',
 export default keys;
